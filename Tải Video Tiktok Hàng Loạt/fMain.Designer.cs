2a1d3d1b29760b0e98613c833e42d66f138c016b
Nguyễn Trung Đức <54103645+trungducnguyen419@users.noreply.github.com>
2022-05-24 14:35:18 +0000

    Add files via upload

---
+++ Tải Video Tiktok Hàng Loạt/fMain.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Tải_Video_Tiktok_Hàng_Loạt
+{
+    partial class fMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fMain));
+            this.tbUrl = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbUrl
+            // 
+            this.tbUrl.Location = new System.Drawing.Point(15, 26);
+            this.tbUrl.Multiline = true;
+            this.tbUrl.Name = "tbUrl";
+            this.tbUrl.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbUrl.Size = new System.Drawing.Size(298, 129);
+            this.tbUrl.TabIndex = 0;
+            this.tbUrl.WordWrap = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 14);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Danh sách link video:";
+            // 
+            // btn
+            // 
+            this.btn.Location = new System.Drawing.Point(108, 161);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(99, 23);
+            this.btn.TabIndex = 2;
+            this.btn.Text = "Tải";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // fMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(327, 189);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbUrl);
+            this.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "fMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tải Video Tiktok Hàng Loạt";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbUrl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn;
+    }
+}
+
---
+++ Tải Video Tiktok Hàng Loạt/fMain.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Forms;
+
+namespace Tải_Video_Tiktok_Hàng_Loạt
+{
+    public partial class fMain : Form
+    {
+        public fMain()
+        {
+            if (!Directory.Exists(Application.StartupPath + "\\video")) Directory.CreateDirectory(Application.StartupPath + "\\video");
+            InitializeComponent();
+            Label.CheckForIllegalCrossThreadCalls = false;
+            Button.CheckForIllegalCrossThreadCalls = false;
+            TextBox.CheckForIllegalCrossThreadCalls = false;
+            Form.CheckForIllegalCrossThreadCalls = false;
+        }
+        bool isStop = false;
+        private void btn_Click(object sender, EventArgs e)
+        {
+            if (btn.Text == "Tải")
+            {
+                tbUrl.Enabled = false;
+                isStop = false;
+                btn.Text = "Dừng";
+                Thread thread = new Thread(() =>
+                {
+                    try
+                    {
+                        var i = 0;
+                        foreach (var url in tbUrl.Lines)
+                        {
+                            if (isStop) return;
+                            i++;
+                            if (!string.IsNullOrEmpty(url))
+                            {
+                                btn.Text = $"Dừng {i}/{tbUrl.Lines.Length}";
+                                var request = (HttpWebRequest)WebRequest.Create($"https://dntik.com/api?type=get-id-video&url={HttpUtility.UrlEncode(url)}");
+                                var response = (HttpWebResponse)request.GetResponse();
+                                var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+                                string id = Regex.Match(responseString, "\"id\":\"(.*?)\"").Groups[1].Value;
+                                if (!string.IsNullOrEmpty(id))
+                                {
+                                    request = (HttpWebRequest)WebRequest.Create($"https://dntik.com/api?type=get-info-video&id={id}");
+                                    request.UserAgent = "TrungDucNguyen";
+                                    response = (HttpWebResponse)request.GetResponse();
+                                    responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+                                    string mp4 = Regex.Match(responseString.Replace(" ", ""), "\"mp4\":\"(.*?)\"").Groups[1].Value;
+                                    Uri uri = new Uri(mp4);
+                                    string filename = System.IO.Path.GetFileName(uri.LocalPath);
+                                    new WebClient().DownloadFile(mp4, Application.StartupPath + "\\video\\" + id + ".mp4");
+                                }    
+                            }
+                        }
+                        tbUrl.Enabled = true;
+                        isStop = true;
+                        btn.Text = "Tải";
+                        return;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Đã xảy ra lỗi: {ex.Message}!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        tbUrl.Enabled = true;
+                        isStop = true;
+                        btn.Text = "Tải";
+                        return;
+                    }
+                });
+                thread.IsBackground = false;
+                thread.Start();
+            }   
+            else
+            {
+                tbUrl.Enabled = true;
+                isStop = true;
+                btn.Text = "Tải";
+            }    
+        }
+    }
+}
