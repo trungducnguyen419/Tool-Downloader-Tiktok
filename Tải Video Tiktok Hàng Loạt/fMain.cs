4d46375d56692a21692c23de2b4c6ffaef089760
Nguyễn Trung Đức <54103645+trungducnguyen419@users.noreply.github.com>
2024-01-02 01:08:46 +0700

    New 1.0.1 (Update API)
    New API Server Download
    New Download Images

--- Tải Video Tiktok Hàng Loạt/fMain.cs
+++ Tải Video Tiktok Hàng Loạt/fMain.cs
@@ -20,6 +20,7 @@ namespace Tải_Video_Tiktok_Hàng_Loạt
         public fMain()
         {
             if (!Directory.Exists(Application.StartupPath + "\\video")) Directory.CreateDirectory(Application.StartupPath + "\\video");
+            if (!Directory.Exists(Application.StartupPath + "\\image")) Directory.CreateDirectory(Application.StartupPath + "\\image");
             InitializeComponent();
             Label.CheckForIllegalCrossThreadCalls = false;
             Button.CheckForIllegalCrossThreadCalls = false;
@@ -46,20 +47,37 @@ namespace Tải_Video_Tiktok_Hàng_Loạt
                             if (!string.IsNullOrEmpty(url))
                             {
                                 btn.Text = $"Dừng {i}/{tbUrl.Lines.Length}";
-                                var request = (HttpWebRequest)WebRequest.Create($"https://dntik.com/api?type=get-id-video&url={HttpUtility.UrlEncode(url)}");
+                                var request = (HttpWebRequest)WebRequest.Create($"https://dntik.ducnguyenfb.com/api?type=get-id-video&url={HttpUtility.UrlEncode(url)}");
                                 var response = (HttpWebResponse)request.GetResponse();
                                 var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
                                 string id = Regex.Match(responseString, "\"id\":\"(.*?)\"").Groups[1].Value;
                                 if (!string.IsNullOrEmpty(id))
                                 {
-                                    request = (HttpWebRequest)WebRequest.Create($"https://dntik.com/api?type=get-info-video&id={id}");
+                                    request = (HttpWebRequest)WebRequest.Create($"https://dntik.ducnguyenfb.com/api?type=get-information-video&id={id}");
+                                    request.Method = "GET";
                                     request.UserAgent = "TrungDucNguyen";
                                     response = (HttpWebResponse)request.GetResponse();
                                     responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
-                                    string mp4 = Regex.Match(responseString.Replace(" ", ""), "\"nowatermark\":\"(.*?)\"").Groups[1].Value;
-                                    Uri uri = new Uri(mp4);
-                                    string filename = System.IO.Path.GetFileName(uri.LocalPath);
-                                    new WebClient().DownloadFile(mp4, Application.StartupPath + "\\video\\" + id + ".mp4");
+                                    responseString = responseString.Replace("\r", "").Replace("\n", "").Replace(" ", "");
+                                    if (responseString.Contains("\"media\":null"))
+                                    {
+                                        string images = Regex.Match(responseString, "\"images\":\\[\"(.*?)\"\\]").Groups[1].Value;
+                                        if (images.Contains("\",\""))
+                                        {
+                                            images = images.Replace("\",\"", "|");
+                                            int j = 0;
+                                            foreach (var img in images.Split('|'))
+                                            {
+                                                j++;
+                                                new WebClient().DownloadFile(img, Application.StartupPath + "\\image\\" + id + $" ({j}).jpeg");
+                                            }    
+                                        }    
+                                    }   
+                                    else
+                                    {
+                                        string media = Regex.Match(responseString, "\"media\":\"(.*?)\"").Groups[1].Value;
+                                        new WebClient().DownloadFile(media, Application.StartupPath + "\\video\\" + id + ".mp4");
+                                    }    
                                 }    
                             }
                         }
